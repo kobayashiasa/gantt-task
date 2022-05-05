b273edc634675709e2e6b80ad1b2a56af643dc31
kobayashiasa <kobayashiasa@gmail.com>
2022-05-05 20:32:47 +0900

    カラムモデルの作成アソシエーションの作成

---
+++ db/migrate/20220505092732_create_columns.rb
@@ -0,0 +1,10 @@
+class CreateColumns < ActiveRecord::Migration[6.0]
+  def change
+    create_table :columns do |t|
+      t.string :column_title, null: false
+      t.references :user, foreign_key: true, null: false
+
+      t.timestamps
+    end
+  end
+end
