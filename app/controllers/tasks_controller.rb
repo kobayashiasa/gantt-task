56a7852a2a65d7ada92d0ca9ae8341b36296cc7b
kobayashiasa <kobayashiasa@gmail.com>
2022-05-08 15:36:58 +0900

    topにタスクを追加するボタンを作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,20 @@
+class TasksController < ApplicationController
+  def new
+    @task = Task.new
+    @column = Column.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:task_title, :task_info, :column_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "top#index"
-  resources :columns, only: [:new, :create, :edit, :update, :destroy] 
+  resources :columns, only: [:new, :create, :edit, :update, :destroy] do
+    resources :task, only: [:new, :create]
+  end
 end
