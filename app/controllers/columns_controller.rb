2c2509b21a86f78ddf10da7ec6cbb8b8d3809552
kobayashiasa <kobayashiasa@gmail.com>
2022-05-08 14:49:02 +0900

    カラム編集機能

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -1,4 +1,5 @@
 class ColumnsController < ApplicationController
+  before_action :set_column, only: [:edit, :update]
   def new
     @column = Column.new
   end
@@ -12,8 +13,23 @@ class ColumnsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @column.update_attributes(column_params)
+      redirect_to root_path
+    else
+      render action: :edit
+    end
+  end
+
   private
   def column_params
     params.require(:column).permit(:column_title).merge(user: current_user)
+  end
+
+  def set_column
+    @column = Column.find(params[:id])
   end
 end
